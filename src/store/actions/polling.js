70eb135499a443d52eab33cf5861486a21326cb8
Debray <debray.peralta12@gmail.com>
2019-10-25 17:10:33 -0300

    Improved SF Skeleton

--- src/store/actions/polling.js
+++ src/store/actions/polling.js
@@ -2,24 +2,54 @@ import { MESSAGE_POLLING } from "./types";
 import client from "../../apiRest";
 
 /**
- * Polling for messages
- * @param {number} offset - The message offset, so the HUB knows which messages to send
- * @param {string} apiKey -  Api key for the HUB
+ * Action that process the message
+ * @param {object} message - A message pulled from a Lumino HUB
  */
-export const messagePolling = (offset, apiKey) => async (
+export const processMessage = (message) => async (
   dispatch,
   getState
 ) => {
   try {
     // NOTE: The big idea of this function is to pull messages from the HUB and dispatch those responses to redux
     // We will handle them and then send the correct responses
-
-    // TODO: return the response data through the redux dispatch
+    // Process the message...
+    // TODO: return the response data through the redux dispatch (Pending discussion of handling these)
     dispatch({
       type: MESSAGE_POLLING,
-      data: "The response data",
+      data: message,
     });
   } catch (error) {
     throw error;
   }
 };
+
+/**
+ * @param {string} apiKey The API Key for the Lumino HUB.
+ * @param {Array<String>} paymentList The paymentIDs to fetch
+ * @returns {Array<Object>} Messages that were pulled from the Lumino HUB
+ */
+export const getMessages = (apiKey, paymentList) => {
+  // TODO: Implementation
+  // This function should be called every X time to pull all the messages of the HUB.
+  // Then it will return the messages corresponding to those payments
+}
+
+/**
+ * 
+ * @param {Array<Object>} messages Objects that have the data necessary to make put request to the HUB
+ * @returns {void} Dispatches data to redux
+ */
+export const sendMessages = (messages) => {
+  // These objects are constructed by us so this will just be a simple put request.
+  // Each object is as follows
+  // const data = {
+  //   message_id: 1,
+  //   message_order: 1,
+  //   sender: "0x....",
+  //   receiver: "0x....",
+  //   message: messageData
+  // }
+  // These will be constructed and passed in the mssages array
+
+
+}
