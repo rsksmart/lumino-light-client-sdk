c31c5f207cbb194b78473f4b6b8435d345d7fc52
Debray <debray.peralta12@gmail.com>
2019-11-30 17:26:40 -0300

    Fix for scret message, change balance calculation

--- src/store/reducers/channelReducer.js
+++ src/store/reducers/channelReducer.js
@@ -13,29 +13,49 @@ const channel = (state = initialState, action) => {
   switch (action.type) {
     case OPEN_CHANNEL:
       const newChannelId = action.channel.channel_identifier;
-      const newChannels = { ...state, [newChannelId]: action.channel };
+      const newChannels = {
+        ...state,
+        [newChannelId]: {
+          ...action.channel,
+          offChainBalance: "0",
+        },
+      };
       return newChannels;
     case SET_CHANNEL_CLOSED:
       const closedChannelId = action.channel.channel_identifier;
-      const channelsModified = { ...state, [closedChannelId]: action.channel };
+      const channelsModified = {
+        ...state,
+        [closedChannelId]: { ...state[closedChannelId], ...action.channel },
+      };
       return channelsModified;
     case NEW_DEPOSIT:
       const depositedChannelId = action.channel.channel_identifier;
       const channelsDeposited = {
         ...state,
-        [depositedChannelId]: { ...action.channel },
+        [depositedChannelId]: {
+          ...state[depositedChannelId],
+          ...action.channel,
+        },
       };
       return channelsDeposited;
     case CHANGE_CHANNEL_BALANCE:
       const { payment } = action;
-      const { channelId, isReceived, amount } = payment;
-      const bigAmount = bigNumberify(amount);
+      const { channelId, isReceived } = payment;
+      // We get the BP
+      const BP = payment.messages[11].message;
+      // We parse the amounts as BN
+      const bigAmount = bigNumberify(BP.transferred_amount);
       const channelBalance = bigNumberify(state[channelId].balance);
-      let newBalance = channelBalance.add(bigAmount);
-      if (!isReceived) newBalance = channelBalance.sub(bigAmount);
+      // We calculate the accumulated, + for reception, - for sending
+      let accumulated = channelBalance.add(bigAmount);
+      if (!isReceived) accumulated = channelBalance.sub(bigAmount);
+      // Return the state with the balances
       return {
         ...state,
-        [channelId]: { ...state[channelId], balance: newBalance.toString() },
+        [channelId]: {
+          ...state[channelId],
+          offChainBalance: accumulated.toString(),
+        },
       };
     default:
       return state;
