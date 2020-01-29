41c1cf51160d3423217851bf4685ff969499e4d3
Debray <debray.peralta12@gmail.com>
2020-01-29 08:40:29 -0300

    Callbacks rewrite to more extensible form

--- src/utils/callbacks.js
+++ src/utils/callbacks.js
@@ -1,13 +1,22 @@
-export const RECEIVED_PAYMENT = "ReceivedPayment";
-export const COMPLETED_PAYMENT = "CompletedPayment";
-export const EXPIRED_PAYMENT = "ExpiredPayment";
+const RECEIVED_PAYMENT = "ReceivedPayment";
+const COMPLETED_PAYMENT = "CompletedPayment";
+const EXPIRED_PAYMENT = "ExpiredPayment";
+const OPEN_CHANNEL = "OpenChannel";
+const REQUEST_CLIENT_ONBOARDING = "RequestClientOnboarding";
+const CLIENT_ONBOARDING_SUCCESS = "ClientOnboardingSuccess";
+const DEPOSIT_CHANNEL = "ChannelDeposit";
 
 export const CALLBACKS = {
   [RECEIVED_PAYMENT]: RECEIVED_PAYMENT,
   [COMPLETED_PAYMENT]: COMPLETED_PAYMENT,
+  [EXPIRED_PAYMENT]: EXPIRED_PAYMENT,
+  [OPEN_CHANNEL]: OPEN_CHANNEL,
+  [REQUEST_CLIENT_ONBOARDING]: REQUEST_CLIENT_ONBOARDING,
+  [CLIENT_ONBOARDING_SUCCESS]: CLIENT_ONBOARDING_SUCCESS,
+  [DEPOSIT_CHANNEL]: DEPOSIT_CHANNEL,
 };
 
-export const newCallbacks = () => {
+const Callbacks = () => {
   const callbacks = {};
 
   const set = (CB, FN) => {
@@ -16,76 +25,12 @@ export const newCallbacks = () => {
 
   const trigger = CB => {
     if (callbacks[CB]) return callbacks[CB]();
-    return console.error("Callback was not set");
+    return () => {};
   };
 
   return { set, trigger };
 };
 
-export const newCbs = newCallbacks();
-
-const Callbacks = () => {
-  // TODO: Rewrite this in a more extensible way
-  let onReceivedPaymentCallback = () => {};
-  let onCompletedPaymentCallback = () => {};
-
-  let onOpenChannel = () => {};
-  let onChannelDeposit = () => {};
-
-  let onRequestClientOnboarding = () => {};
-  let onClientOnboardingSuccess = () => {};
-
-  const setOnReceivedPaymentCallback = fn => (onReceivedPaymentCallback = fn);
-  const setOnCompletedPaymentCallback = fn => (onCompletedPaymentCallback = fn);
-
-  const setOnOpenChannelCallback = fn => (onOpenChannel = fn);
-  const setOnChannelDepositCallback = fn => (onChannelDeposit = fn);
-
-  const setOnRequestClientOnboarding = fn => (onRequestClientOnboarding = fn);
-  const setOnClientOnboardingSuccess = fn => (onClientOnboardingSuccess = fn);
-
-  // Payments trigger
-
-  const triggerOnReceivedPaymentCallback = payment =>
-    onReceivedPaymentCallback(payment);
-
-  const triggerOnCompletedPaymentCallback = payment =>
-    onCompletedPaymentCallback(payment);
-
-  // Channels trigger
-
-  const triggerOnOpenChannel = channel => onOpenChannel(channel);
-  const triggerOnDepositChannel = channel => onChannelDeposit(channel);
-
-  // Onboarding trigger
-
-  const triggerOnRequestClientOnboarding = addr =>
-    onRequestClientOnboarding(addr);
-  const triggerOnClientOnboardingSuccess = addr =>
-    onClientOnboardingSuccess(addr);
-
-  const callbacks = {
-    trigger: {
-      triggerOnCompletedPaymentCallback,
-      triggerOnReceivedPaymentCallback,
-      triggerOnOpenChannel,
-      triggerOnDepositChannel,
-      triggerOnRequestClientOnboarding,
-      triggerOnClientOnboardingSuccess,
-    },
-    set: {
-      setOnCompletedPaymentCallback,
-      setOnReceivedPaymentCallback,
-      setOnOpenChannelCallback,
-      setOnChannelDepositCallback,
-      setOnRequestClientOnboarding,
-      setOnClientOnboardingSuccess,
-    },
-  };
-
-  return callbacks;
-};
-
-const callbacks = Callbacks();
+export const callbacks = Callbacks();
 
 export default callbacks;
