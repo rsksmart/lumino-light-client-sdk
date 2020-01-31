e12e707befa42d17dfe72ba8f689350f755eebce
mmartinez@infuy.com <mmartinez@infuy.com>
2020-01-31 12:00:26 -0300

    Fix message_value_type parameter on secretRequest

--- src/store/actions/payment.js
+++ src/store/actions/payment.js
@@ -261,6 +261,7 @@ export const putSecretRequest = (msg, payment, isReception = false) => async (
     message_order: 5,
     sender: getAddress(sender),
     receiver: getAddress(receiver),
+    message_type_value: PAYMENT_SUCCESSFUL,
     message: {
       type: MessageType.SECRET_REQUEST,
       message_identifier: msg.message_identifier,
@@ -268,7 +269,6 @@ export const putSecretRequest = (msg, payment, isReception = false) => async (
       amount: payment.amount,
       expiration: msg.expiration,
       secrethash: payment.secret_hash,
-      message_type_value: PAYMENT_SUCCESSFUL
     },
   };
   const dataToSign = getDataToSignForSecretRequest(body.message);
