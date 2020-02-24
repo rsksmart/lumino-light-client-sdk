c6f0cb5e3675d170c70c0f63a2ba473ed1c0bdf7
Debray <debray.peralta12@gmail.com>
2020-02-24 09:03:06 -0300

    Callbacks unit test

---
+++ test/callbacks/index.test.js
@@ -0,0 +1,18 @@
+import callbacks, { CALLBACKS } from "../../src/utils/callbacks";
+
+describe("Callback tests", () => {
+  test("Can set a Callback of 1 parameter and trigger it", () => {
+    const stubFn = jest.fn();
+    callbacks.set(CALLBACKS.OPEN_CHANNEL, stubFn);
+    callbacks.trigger(CALLBACKS.OPEN_CHANNEL, "123");
+    expect(stubFn).toBeCalledWith("123");
+  });
+
+  test("Can set a Callback of 2 parameters and trigger it", () => {
+    const stubFn = jest.fn();
+    callbacks.set(CALLBACKS.FAILED_OPEN_CHANNEL, stubFn);
+    const error = new Error("Failure");
+    callbacks.trigger(CALLBACKS.FAILED_OPEN_CHANNEL, "123", error);
+    expect(stubFn).toBeCalledWith("123", error);
+  });
+});
