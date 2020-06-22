be29c3bbca3ac39e7ab28ff505c18ba74ff65fc1
Debray Peralta <debray.peralta@gmail.com>
2020-06-22 09:50:21 -0300

    Lumino config reasign at top, pass api key to init store

--- src/Lumino/index.js
+++ src/Lumino/index.js
@@ -29,7 +29,12 @@ const Lumino = () => {
    */
   const init = async (luminoHandler, storage, configParams) => {
     if (!store) {
-      store = await Store.initStore(storage, luminoHandler);
+      luminoConfig = { ...luminoConfig, ...configParams };
+      store = await Store.initStore(
+        storage,
+        luminoHandler,
+        luminoConfig.apiKey
+      );
       actions = Store.bindActions(Actions, store.dispatch);
       // Set address
       store.dispatch({
@@ -44,7 +49,6 @@ const Lumino = () => {
       const changesHook = fn => store.subscribe(fn);
       const luminoInternalState = store.getState();
       const getLuminoInternalState = () => store.getState();
-      luminoConfig = { ...luminoConfig, ...configParams };
       client.defaults.baseURL = luminoConfig.hubEndpoint;
       notifier.defaults.baseURL = luminoConfig.notifierEndPoint;
       actions = { ...actions };
