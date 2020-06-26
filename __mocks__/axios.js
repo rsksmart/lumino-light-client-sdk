e8d52c44d22bda2b7ccb2d41dc842ecf001d730f
Debray Peralta <debray.peralta@gmail.com>
2020-06-26 09:50:47 -0300

    Added canceltoken mock

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -3,4 +3,13 @@ const mockAxios = jest.genMockFromModule("axios");
 // this is the key to fix the axios.create() undefined error!
 mockAxios.create = jest.fn(() => mockAxios);
 
+mockAxios.CancelToken = {
+  source: jest.fn(() => {
+    return {
+      token: "CancelToken",
+      cancel: () => jest.fn(),
+    };
+  }),
+};
+
 export default mockAxios;
