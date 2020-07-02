e37011c13240e6758c22b54d23c15352e381f7d7
Debray Peralta <debray.peralta@gmail.com>
2020-07-02 13:17:44 -0300

    Change new deposit amount logic check

--- src/store/actions/deposit.js
+++ src/store/actions/deposit.js
@@ -20,7 +20,23 @@ export const createDeposit = params => async (dispatch, getState, lh) => {
   const { partner, channelId, tokenAddress } = params;
   let { amount } = params;
   const channel = getChannelByIdAndToken(channelId, tokenAddress);
-  amount = new BigNumber(amount).plus(channel.total_deposit).toString();
+  const { offChainBalance, total_deposit } = channel;
+  amount = new BigNumber(amount).plus(offChainBalance).toString();
+  const currentTotalDeposit = new BigNumber(total_deposit);
+
+  // We check that the new deposit is enough, if not, we trigger a proper callback
+  if (currentTotalDeposit.isGreaterThanOrEqualTo(amount)) {
+    const valueDifference = currentTotalDeposit.minus(amount).toString();
+    const error = new Error(
+      `The current deposit is ${currentTotalDeposit.toString()} and the final amount is ${amount}, the amount must be greater than the deposit, difference ${valueDifference}`
+    );
+    return Lumino.callbacks.trigger(
+      CALLBACKS.DEPOSIT_CHANNEL_VALUE_TOO_LOW,
+      { currentDeposit: total_deposit, valueDifference, amount },
+      error
+    );
+  }
+
   try {
     const clientAddress = getState().client.address;
 
@@ -40,7 +56,7 @@ export const createDeposit = params => async (dispatch, getState, lh) => {
       signed_close_tx: "",
     };
     const url = `light_channels/${tokenAddress}/${clientAddress}/${partner}`;
-    Lumino.callbacks.trigger(CALLBACKS.REQUEST_DEPOSIT_CHANNEL, channel);
+    Lumino.callbacks.trigger(CALLBACKS.REQUEST_DEPOSIT_CHANNEL, {...channel, amount});
     const res = await client.patch(url, { ...requestBody });
     dispatch({
       type: NEW_DEPOSIT,
