db19a486effe066f9506de9b374e5364f905d36e
Debray Peralta <debray.peralta@gmail.com>
2020-09-14 17:14:55 -0300

    Fix secret register script

--- src/scripts/secret.js
+++ src/scripts/secret.js
@@ -8,17 +8,18 @@ import Lumino from "../Lumino/index";
 
 export const createRegisterSecretTx = async params => {
   const { rskEndpoint, chainId } = Lumino.getConfig();
+  const { secretRegistryAddress } = params;
   const web3 = new Web3(rskEndpoint);
   const secretRegistry = new web3.eth.Contract(
     secretRegistryABI,
-    params.secretRegistryAddress
+    secretRegistryAddress
   );
   const txCount = await web3.eth.getTransactionCount(params.address);
   const registerTx = {
     nonce: web3.utils.toHex(txCount),
     gasPrice: params.gasPrice || DEFAULT_GAS_PRICE,
     gasLimit: params.gasLimit || DEFAULT_GAS_LIMIT,
-    to: params.tokenNetworkAddress,
+    to: secretRegistryAddress,
     value: "0x00",
     data: secretRegistry.methods.registerSecret(params.secret).encodeABI(),
     chainId,
