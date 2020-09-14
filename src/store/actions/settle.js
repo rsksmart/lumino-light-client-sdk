d27753438109f0231797033188e921ea050e0d82
Debray Peralta <debray.peralta@gmail.com>
2020-09-14 13:58:26 -0300

    Settlement action

---
+++ src/store/actions/settle.js
@@ -0,0 +1,31 @@
+import client from "../../apiRest";
+import { createSettleTx } from "../../scripts/settle";
+import { chkSum } from "../../utils/functions";
+import resolver from "../../utils/handlerResolver";
+import { getTokenAddressByTokenNetwork } from "../functions/tokens";
+
+export const settleChannel = data => async (dispatch, getState, lh) => {
+  const { txParams } = data;
+  const unsignedTx = await createSettleTx(txParams);
+  const signedTx = await resolver(unsignedTx, lh);
+  const requestBody = {
+    state: "waiting_for_settle",
+    signed_settle_tx: signedTx,
+    signed_approval_tx: "",
+    signed_close_tx: "",
+    signed_deposit_tx: "",
+  };
+
+  const { tokenNetworkAddress } = txParams;
+  const tokenAddress = getTokenAddressByTokenNetwork(tokenNetworkAddress);
+  const { creatorAddress, partnerAddress } = data;
+  const url = `light_channels/${chkSum(
+    tokenAddress
+  )}/${creatorAddress}/${partnerAddress}`;
+  try {
+    const res = await client.patch(url, { ...requestBody });
+    console.log("Settlement successful!", res.data);
+  } catch (error) {
+    console.error("Settlement failed!", error);
+  }
+};
