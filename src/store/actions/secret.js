77c3b5a12d9895d475bf6e6bd4e4dd19e42a0786
Debray Peralta <debray.peralta@gmail.com>
2020-09-16 11:46:34 -0300

    Dispatch is registering and registered secrets

--- src/store/actions/secret.js
+++ src/store/actions/secret.js
@@ -1,23 +1,42 @@
 import { Lumino } from "../..";
 import client from "../../apiRest";
 import { createRegisterSecretTx } from "../../scripts/secret";
-import { getRandomBN } from "../../utils/functions";
 import resolver from "../../utils/handlerResolver";
+import { REGISTERING_ON_CHAIN_SECRET } from "./types";
 
 export const registerSecret = data => async (dispatch, getState, lh) => {
-  const { secret, secretRegistryAddress } = data;
+  const { secret, secretRegistryAddress, paymentId } = data;
   const { address } = Lumino.getConfig();
   const txParams = { secret, secretRegistryAddress, address };
   const unsignedTx = await createRegisterSecretTx(txParams);
   const signedTx = await resolver(unsignedTx, lh);
   const url = "payments_light/register_onchain_secret";
-  console.log(signedTx);
   try {
-    const body = { signed_tx: signedTx, message_id: getRandomBN() };
+    dispatch(registeringOnChainSecret(true, paymentId));
+    const body = { signed_tx: signedTx };
     const res = await client.post(url, body);
     console.log("Success sending onchain secret", res);
+    dispatch(registeredOnChainSecret(paymentId));
   } catch (error) {
     console.error("Error when registering onchain secret", error);
+    dispatch(registeringOnChainSecret(false, paymentId));
     return false;
   }
 };
+
+const registeringOnChainSecret = (
+  registeringOnChainSecret,
+  paymentId
+) => dispatch =>
+  dispatch({
+    type: REGISTERING_ON_CHAIN_SECRET,
+    registeringOnChainSecret,
+    paymentId,
+  });
+
+const registeredOnChainSecret = paymentId => dispatch =>
+  dispatch({
+    type: REGISTERING_ON_CHAIN_SECRET,
+    registeringOnChainSecret,
+    paymentId,
+  });
