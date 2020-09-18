e8bdac888de995436657d264cebfe11257312115
Debray Peralta <debray.peralta@gmail.com>
2020-09-18 16:50:21 -0300

    Removed unused values in client reducer test

--- test/store/reducers/client.test.js
+++ test/store/reducers/client.test.js
@@ -6,9 +6,6 @@ import {
 } from "../../../src/store/actions/types";
 import reducer from "../../../src/store/reducers/clientReducer";
 
-// const mockAddr = "0xFb783358Ff2b40630B112e3B937f0c43C1Ab2172";
-// const mockNetwork = "0x931A46774dFDC44aac1D6eCa15930b6c3895dD7a";
-
 const mockApiKey = "AbCdER90123";
 
 const initialState = {
