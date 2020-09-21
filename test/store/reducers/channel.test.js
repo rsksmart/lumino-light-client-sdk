92ccf7a46034ec882b56d36d58d134984a8958ab
Debray Peralta <debray.peralta@gmail.com>
2020-09-21 09:55:04 -0300

    Channel Reducer tests part 2

--- test/store/reducers/channel.test.js
+++ test/store/reducers/channel.test.js
@@ -1,14 +1,23 @@
 import {
+  SET_CHANNEL_SETTLED,
   SET_CHANNEL_UNLOCKED,
+  SET_IS_SETTLING,
   SET_IS_UNLOCKING,
 } from "../../../src/store/actions/types";
 import reducer from "../../../src/store/reducers/channelReducer";
-import { CHANNEL_UNLOCKED } from "../../../src/config/channelStates";
+import {
+  CHANNEL_SETTLED,
+  CHANNEL_UNLOCKED,
+} from "../../../src/config/channelStates";
 
 const mockAddr = "0xFb783358Ff2b40630B112e3B937f0c43C1Ab2172";
 const mockToken = "0x931A46774dFDC44aac1D6eCa15930b6c3895dD7a";
 
 const initialState = {};
+
+const getChannelKey = action => {
+  return `${action.data.channel_identifier}-${action.data.token_address}`;
+};
 
 describe("Channel reducer", () => {
   it("should return initial state", () => {
@@ -25,7 +34,7 @@ describe("Channel reducer", () => {
         channel_identifier: 1,
       },
     };
-    const channelKey = `${action.data.channel_identifier}-${action.data.token_address}`;
+    const channelKey = getChannelKey(action);
     const state = {
       [channelKey]: {
         balance: 500,
@@ -53,7 +62,7 @@ describe("Channel reducer", () => {
         isUnlocking: false,
       },
     };
-    const channelKey = `${action.data.channel_identifier}-${action.data.token_address}`;
+    const channelKey = getChannelKey(action);
     const state = {
       [channelKey]: {
         balance: 500,
@@ -71,31 +80,61 @@ describe("Channel reducer", () => {
     expect(red).toEqual(expected);
   });
 
-  
+  it("Should handle SET_IS_SETTLING", () => {
+    const action = {
+      type: SET_IS_SETTLING,
+      data: {
+        token_address: mockToken,
+        channel_identifier: 1,
+        isSettling: false,
+      },
+    };
+    const channelKey = getChannelKey(action);
+    const state = {
+      [channelKey]: {
+        balance: 500,
+        isSettling: true,
+      },
+    };
+    const red = reducer(state, action);
+    const expected = {
+      ...state,
+      [channelKey]: {
+        ...state[channelKey],
+        isSettling: false,
+      },
+    };
+    expect(red).toEqual(expected);
+  });
 
-  //   it("Should handle SET_LATEST_INTERNAL_MSG_ID", () => {
-  //     const action = {
-  //       type: SET_LATEST_INTERNAL_MSG_ID,
-  //       id: 404,
-  //     };
-  //     const red = reducer(undefined, action);
-  //     const expected = {
-  //       ...initialState,
-  //       internal_msg_id: 404,
-  //     };
-  //     expect(red).toEqual(expected);
-  //   });
+  it("Should handle SET_CHANNEL_SETTLED", () => {
+    const action = {
+      type: SET_CHANNEL_SETTLED,
+      data: {
+        token_address: mockToken,
+        partner_address: mockAddr,
+        channel_identifier: 1,
+      },
+    };
+    const channelKey = getChannelKey(action);
+    const state = {
+      [channelKey]: {
+        balance: 500,
+        isSettled: false,
+        isSettling: true,
+      },
+    };
+    const red = reducer(state, action);
+    const expected = {
+      ...state,
+      [channelKey]: {
+        ...state[channelKey],
+        sdk_status: CHANNEL_SETTLED,
+        isSettled: true,
+        isSettling: false,
+      },
+    };
+    expect(red).toEqual(expected);
+  });
 
-  //   it("Should handle SET_LAST_NOTIFICATION_ID", () => {
-  //     const action = {
-  //       type: SET_LAST_NOTIFICATION_ID,
-  //       id: 405,
-  //     };
-  //     const red = reducer(undefined, action);
-  //     const expected = {
-  //       ...initialState,
-  //       lastNotificationId: 405,
-  //     };
-  //     expect(red).toEqual(expected);
-  //   });
 });
