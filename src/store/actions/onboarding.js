f6680ae657cc6366e7491b6cfb42220713c93bd9
Marcos Martínez <martinez.marcos@live.com>
2020-12-10 15:31:36 -0300

    Feature/rlp 859/rif comms onboarding impl (#105)
    * wip
    
    * wip
    
    * renamed transport mode to transport type

--- src/store/actions/onboarding.js
+++ src/store/actions/onboarding.js
@@ -17,30 +17,51 @@ export const onboardingClient = () => async (dispatch, getState, lh) => {
     client.defaults.headers = {};
     const urlOnboard = "light_clients/";
     const onboardReq = await client.get(urlOnboard, { params: { address } });
-    // We get the data
+
+    // Get the onboarding data from the hub
     const {
+      transport_type,
       display_name_to_sign,
       seed_retry,
       password_to_sign,
     } = onboardReq.data;
-    // Sign it
-    const signed_display_name = await resolver(display_name_to_sign, lh, true);
-    const signed_password = await resolver(password_to_sign, lh, true);
-    const signed_seed_retry = await resolver(seed_retry, lh, true);
-    // Prepare a body for the request with all the required data
-    const body = {
-      registration_data : {
-        address,
-        signed_password,
-        signed_display_name,
-        signed_seed_retry,
-        password: password_to_sign,
-        display_name: display_name_to_sign,
-        seed_retry,
-      },
-    };
+
+    // Check hub current transport implementation
+    let register_body = {};
+    if (transport_type === "matrix") {
+      // Sign the onboarding matrix data
+      const signed_display_name = await resolver(
+        display_name_to_sign,
+        lh,
+        true
+      );
+      const signed_password = await resolver(password_to_sign, lh, true);
+      const signed_seed_retry = await resolver(seed_retry, lh, true);
+      // Prepare a body for the request with all the required data
+      register_body = {
+        registration_data: {
+          address,
+          signed_password,
+          signed_display_name,
+          signed_seed_retry,
+          password: password_to_sign,
+          display_name: display_name_to_sign,
+          seed_retry,
+        },
+      };
+    } else if (transport_type === "rif-comms") {
+      register_body = {
+        registration_data: {
+          address,
+        },
+      };
+    } else {
+      throw new Error("Onboadring error: hub isnt using rif comms nor matrix");
+    }
+
+    // Register the light client
     dispatch({ type: REQUEST_CLIENT_ONBOARDING, address });
-    const resOnboard = await client.post(urlOnboard, body);
+    const resOnboard = await client.post(urlOnboard, register_body);
     // We extract the api key, set it as a header and store it on redux
     const { api_key } = resOnboard.data;
 
